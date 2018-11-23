ede1f3e0e5faf13d3ca02aafa613fd7bb9936cb6
Larsbl00 <larsbloemers@gmail.com>
2018-11-23 13:26:55 +0100

    Makefile supports object detection

--- computerVision/src/main.cpp
+++ computerVision/src/main.cpp
@@ -3,11 +3,19 @@
 
 #include <ctime>
 #include <fstream>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <unistd.h>
 #include <iostream>
 #include <thread>
 
-using namespace std;
+std::string FaceCascadeFileName = "./haarcascades/haarcascade_frontalface_alt.xml";
+std::string EyesCascadeFileName = "./haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+cv::CascadeClassifier FaceCascade;
+cv::CascadeClassifier EyesCascade;
 
 int main ( int argc,char **argv ) 
 {
